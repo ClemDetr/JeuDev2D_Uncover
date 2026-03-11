0f8e689f9b7233b282c94346b4f92e9aabd880de
ClemDetr <clemence.detroyat@gmail.com>
2026-03-11 11:20:23 +0100

    add sprites and scene base

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 import kaplay from "https://unpkg.com/kaplay@3001.0.19/dist/kaplay.mjs";
 kaplay();
+
+
+"Sprites load"
+loadSprite('bg-base',"./assets/battleback10.png")
+
+
+setBackground(BLACK)
+
+scene('base', () => {
+    add([
+        sprite('bg-base')
+    ])
+});
+
+go('base');
