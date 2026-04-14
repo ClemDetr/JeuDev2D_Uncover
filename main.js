51753b29b699634dd761f01692b7accf03b43556
ClemDetr <clemence.detroyat@gmail.com>
2026-04-14 09:46:54 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ kaplay();
 
 
 //Sprites Loads
-loadSprite('bg-base',"./assets/battleback10.png")
+loadSprite('OF',"./assets/249-OF.JPG")
 loadSprite('arrow',"./assets/arrow.png")
 loadSprite('parrot',"./assets/parrot.png")
 loadSprite('chest',"./assets/chest.png")
@@ -12,6 +12,7 @@ loadSprite('openChest',"./assets/chestOpen.png")
 loadSprite('star',"./assets/star.png")
 loadSprite('EM',"./assets/253-EM.JPG")
 loadSprite('Mus1',"./assets/254-Mus1.JPG")
+loadSprite('RP',"./assets/252-RP.JPG")
 
 setBackground(BLACK)
 
@@ -44,8 +45,8 @@ scene('EM', () => {
         ])
     };
 
-    rightArrow.onClick(() => (go('Mus1')))
-
+    rightArrow.onClick(() => (go('Mus1')));
+    leftArrow.onClick(() => (go('RP')));
 });
 
 scene('Mus1', () => {
@@ -55,64 +56,35 @@ scene('Mus1', () => {
     ]);
 
 
-    const parrot = add([
-        sprite('parrot'),
-        scale(4),
-        pos(500,300),
-        anchor('center'),
-        area()
-    ])
+    
 
-    const rightArrow = add([
+    const arrow = add([
         sprite('arrow'),
         pos(950,300),
         area()
     ]);
 
     
-    const chest = add([
-        sprite('chest'),
-        pos(20,300),
+    
+    arrow.onClick(() =>(go('EM')));
+
+});
+
+scene('RP', () => {
+    add([
+        sprite('RP'),
+        scale(0.3)
+    ]);
+
+    const leftArrow = add([
+        sprite('arrow',{
+            flipX: true
+        }),
+        pos(30,300),
         area()
     ]);
 
-    if (GOTSPECIALITEM == true) {
-        const specialItem = add([
-            sprite('star'),
-            pos(0,0),
-        ])
-        chest.sprite = 'openChest'
-    }
-    rightArrow.onClick(() =>(go('EM')));
-
-    parrot.onClick(() => {
-        const backtext = add([
-            rect(1000,100),
-            pos(0,500),
-            color(255,255,255)
-        ])
-        const message = add([
-            text('Parrot : You found me !'),
-            color(0,0,0),
-            pos(0, 550),
-        ]);
-
-        wait(2, () => {
-            message.destroy()
-            backtext.destroy()
-        });
-    })
-
-    chest.onClick(() => {
-        chest.sprite = 'openChest',
-        GOTSPECIALITEM = true;
-        const specialItem = add([
-            sprite('star'),
-            pos(0,0),
-        ])
-    });
+    leftArrow.onClick(() => (go('EM')));
 });
-
-scene('rightbase')
 
 go('EM');
